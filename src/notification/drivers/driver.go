327410bef6a6a4970b135ad6afe1d238d17d8a0b
Sercan Değirmenci <sercan@panteon.com.tr>
2015-08-20 17:40:26 +0300

    driver prototype

--- src/notification/drivers/driver.go
+++ src/notification/drivers/driver.go
@@ -1,1 +1,24 @@
 package drivers
+import "fmt"
+
+type Driver interface{
+
+}
+
+type RegisteredDriver struct {
+	New func() (Driver, error)
+}
+
+var drivers map[string]*RegisteredDriver
+
+func init() {
+	drivers = make(map[string]*RegisteredDriver)
+}
+
+func Register(name string, rd *RegisteredDriver) error {
+	if _, ext := drivers[name]; ext {
+		return fmt.Errorf("Name already registered %s", name)
+	}
+	drivers[name] = rd;
+	return nil
+}
--- src/notification/drivers/sendgrid/sendgrid.go
+++ src/notification/drivers/sendgrid/sendgrid.go
@@ -1,1 +1,14 @@
 package sendgrid
+
+import "notification/drivers"
+
+
+func init() {
+	drivers.Register("sendgrid", &drivers.RegisteredDriver{
+		New:newDriver,
+	})
+}
+
+func newDriver() (drivers.Driver, error) {
+	return nil, nil
+}
